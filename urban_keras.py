e159696d6e540329c8b2332456394d5d4f020d59
Chintan Thakkar <32512012+chintant94@users.noreply.github.com>
2018-04-23 12:10:43 +0530

    Sequential model
    This program classifies different sounds made by coffee machine and identifies the corresponding drink being made by the machine, based on the pattern of sound produced by it while making the drink.

---
+++ urban_keras.py
@@ -0,0 +1,109 @@
+import librosa
+import librosa.display
+import csv
+import os
+import subprocess
+import numpy as np
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+
+
+def mp3towav(dir):
+    for filename in os.listdir(dir):
+        if(dir.split('\\')[-2]=='Train'):
+            subprocess.call(['ffmpeg','-i',dir+filename,dir[:-6]+'Train16bit\\'+filename[:-4]+'.wav'])
+        else:
+            subprocess.call(['ffmpeg','-i',dir+filename,dir[:-5]+'Test16bit\\'+filename[:-4]+'.wav'])
+       
+    
+mp3towav('C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\train\\Train\\')  
+mp3towav('C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\test\\Test\\')  
+
+
+np.random.seed(7)
+tr_dataset=pd.read_csv("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\train\\train.csv")
+ts_dataset=pd.read_csv("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\test\\test.csv")
+
+
+def loadFileAndFeature(dir):
+    mfccs=[]
+    label=[]
+    reader=csv.reader(open(dir+"\\"+dir.split('\\')[-1].lower()+".csv","r"),delimiter=',')
+    next(reader)
+    if(dir.split('\\')[-1]=='train'):
+        for row in reader:  
+            y, sr = librosa.load(dir+'\\T'+dir.split('\\')[-1][1:]+'16bit\\'+row[0]+'.wav')
+            mfccs.append(np.mean(librosa.feature.mfcc(y,sr,n_mfcc=40).T,axis=0))
+            label.append(row[1])
+    else:
+        for row in reader:  
+            y, sr = librosa.load(dir+'\\T'+dir.split('\\')[-1][1:]+'16bit\\'+row[0]+'.wav')
+            mfccs.append(np.mean(librosa.feature.mfcc(y,sr,n_mfcc=40).T,axis=0))
+    return label,mfccs
+
+
+#def loadFileAndFeature1(row):
+#    y, sr = librosa.load(dir+'\\T'+dir.split('\\')[-1][1:]+'16bit\\'+str(row.ID)+'.wav')
+#    mfccs=(np.mean(librosa.feature.mfcc(y,sr,n_mfcc=40).T,axis=0))
+#    label=(row.Class)
+#    return [label,mfccs]
+
+#tr=tr_dataset.apply(loadFileAndFeature,axis=1)
+#tr.columns('label','feature')
+
+tr_label, tr_features=loadFileAndFeature("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\train")
+tr_features=np.array(tr_features)
+tr_label=np.array(tr_label)
+
+#ts=ts_dataset.apply(loadFileAndFeature,axis=1)
+#ts.columns=['label','feature']
+
+ts_label, ts_features=loadFileAndFeature("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\test")
+ts_features=np.array(ts_features)
+ts_label=np.array(ts_label)
+
+lb=LabelEncoder()
+encoded_label=np_utils.to_categorical(lb.fit_transform(tr_label))
+distinct_labels=encoded_label.shape[1]
+
+del model
+
+model=Sequential()
+model.add(Dense(100,input_shape=(40,),activation='relu'))
+model.add(Dense(100,activation='relu'))
+model.add(Dense(distinct_labels,activation='softmax'))
+
+model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam')
+model.fit(tr_features,encoded_label,epochs=30,validation_split=0.2)
+
+predicted_class = model.predict_classes(ts_features)
+output=lb.inverse_transform(predicted_class)
+print(output)
+
+x=output.tolist()
+x=pd.DataFrame(x)
+x.columns=['Class']
+
+df=pd.concat([x,ts_dataset],axis=1)
+
+df.to_csv("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\test\\output.csv",sep=',',index=False)
+
+#with open("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\test\\output.csv","w") as file:
+#    csv_out=csv.writer(file)
+#    for row in df:
+#        csv_out.writerow(row)
+
+#To add 2 lists 2 a single list
+#temp=list(zip(x,ts_dataset))
+        
+        
+#Write a list to CSV file
+#with open("C:\\Users\\I333690\\Desktop\\Fellowship\\Audio_files\\Dataset\\urban_sounds\\test\\output.csv","w") as file:
+#    for line in output:
+#        file.write(line)
+#        file.write('\n')
+
+    
